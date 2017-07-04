45cb2e5d9724de50a4b264f604d2a10f6b00a45d
Patsko <luispatsko@yahoo.com.br>
2017-07-03 21:34:48 -0300

    Code changes due to a hardware test - AX0 changed from PA1 to PA0, crosspoint switch control modified to be able to switch between bypass, one of three effects or mute. "wsp" file removed from repository and added to gitignore - contains only ZDS workspace info

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 /*
 
-PA1 - AX0
+PA1 - AX0       OBS: alterado para PA0
 PA2 - AX1
 PA6 - AX2
 PA7 - AX3
@@ -23,7 +23,9 @@ PC5 - Strobe
 PB0 - CS
 
 PB1 - botão
-PC3 - LED
+PC2 - LED1
+PC3 - LED2
+
 */
 
 
@@ -43,7 +45,7 @@ void MCU_INIT () {
     };
 
     // Crosspoint switch addresses
-    ZHAL_GPIO_Config_Pin(ZHAL_GPIO_A, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_6 | GPIO_PIN_7, &gpio_config);
+    ZHAL_GPIO_Config_Pin(ZHAL_GPIO_A, GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_6 | GPIO_PIN_7, &gpio_config);
     ZHAL_GPIO_Config_Pin(ZHAL_GPIO_C, GPIO_PIN_0 | GPIO_PIN_6 | GPIO_PIN_7, &gpio_config);
 
     // Crosspoint switch serial
@@ -52,7 +54,7 @@ void MCU_INIT () {
 
     // LED
     gpio_config.Open_Drain = DISABLE;
-    ZHAL_GPIO_Config_Pin(ZHAL_GPIO_C, GPIO_PIN_3, &gpio_config);
+    ZHAL_GPIO_Config_Pin(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3, &gpio_config);
 
     // botão
     gpio_config.Direction = ZHAL_GPIO_INPUT;
@@ -62,25 +64,52 @@ void MCU_INIT () {
 
 }
 
-void CROSSPOINT_SWITCH_CONTROL (unsigned char sw, unsigned char status) {
+void CROSSPOINT_SWITCH_CONTROL (unsigned char x, unsigned char y, unsigned char status) {
 
     ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_5);    // Strobe
     ZHAL_GPIO_Reset_Output(ZHAL_GPIO_B, GPIO_PIN_0);    // CS
 
     ZHAL_GPIO_Set_Output(ZHAL_GPIO_B, GPIO_PIN_0);    // CS
     
-    switch (sw) {
-    case 0: // Sets X0-Y0 switch
-        ZHAL_GPIO_Reset_Output(ZHAL_GPIO_A, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_6 | GPIO_PIN_7);
-        ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_0 | GPIO_PIN_6 | GPIO_PIN_7);
+    ZHAL_GPIO_Reset_Output(ZHAL_GPIO_A, GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_6 | GPIO_PIN_7);
+    ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_0 | GPIO_PIN_6 | GPIO_PIN_7);
+
+    switch (x) {
+    case 0:
         break;
-    case 1: // Sets X1-Y1 switch
-        ZHAL_GPIO_Reset_Output(ZHAL_GPIO_A, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_6 | GPIO_PIN_7);
-        ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_0 | GPIO_PIN_6 | GPIO_PIN_7);
-        ZHAL_GPIO_Set_Output(ZHAL_GPIO_A, GPIO_PIN_1);
-        ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_0);
+    case 1:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_A, GPIO_PIN_0);
+        break;
+    case 2:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_A, GPIO_PIN_2);
+        break;
+    case 3:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_A, GPIO_PIN_0 | GPIO_PIN_2);
+        break;
+    case 4:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_A, GPIO_PIN_6);
+        break;
+    case 15:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_A, GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_6 | GPIO_PIN_7);
         break;
     }
+    switch (y) {
+    case 0:
+        break;
+    case 1:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_0);
+        break;
+    case 2:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_6);
+        break;
+    case 3:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_0 | GPIO_PIN_6);
+        break;
+    case 4:
+        ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_7);
+        break;
+    }
+
 
     if (status == 0) {  // Data
         ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_4);
@@ -146,26 +175,58 @@ void main () {
     MCU_INIT(); //rotinas de inicialização
 
     MATRIX.Status = 0;
+    CROSSPOINT_SWITCH_CONTROL(0, 0, 1);
     
     while (1){
 
         if (BUTTON()) {
-            if (MATRIX.Status == 0) {
-                // Closes X1-Y1 switch
-                CROSSPOINT_SWITCH_CONTROL(0, 0);
-                CROSSPOINT_SWITCH_CONTROL(1, 1);
-                
-                ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_3);
-                
-                MATRIX.Status = 1;
-            } else {
-                // Closes X0-Y0 switch
-                CROSSPOINT_SWITCH_CONTROL(0, 1);
-                CROSSPOINT_SWITCH_CONTROL(1, 0);
-
-                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_3);
-                
+            MATRIX.Status++;
+            if (MATRIX.Status > 4) {
                 MATRIX.Status = 0;
+            }
+
+            switch (MATRIX.Status) {
+            case 0: // bypass - closes X0-Y0
+                CROSSPOINT_SWITCH_CONTROL(0, 0, 1);
+
+                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                break;
+            case 1: // effect 1 - closes X0-Y1 and X2-Y0
+                CROSSPOINT_SWITCH_CONTROL(0, 0, 0);
+
+                CROSSPOINT_SWITCH_CONTROL(0, 1, 1);
+                CROSSPOINT_SWITCH_CONTROL(2, 0, 1);
+
+                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_2);
+                break;
+            case 2: // effect 2 - closes X0-Y2 and X3-Y0
+                CROSSPOINT_SWITCH_CONTROL(0, 1, 0);
+                CROSSPOINT_SWITCH_CONTROL(2, 0, 0);
+
+                CROSSPOINT_SWITCH_CONTROL(0, 2, 1);
+                CROSSPOINT_SWITCH_CONTROL(3, 0, 1);
+
+                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_3);
+                break;
+            case 3: // effect 3 - closes X0-Y3 and X4-Y0
+                CROSSPOINT_SWITCH_CONTROL(0, 2, 0);
+                CROSSPOINT_SWITCH_CONTROL(3, 0, 0);
+
+                CROSSPOINT_SWITCH_CONTROL(0, 3, 1);
+                CROSSPOINT_SWITCH_CONTROL(4, 0, 1);
+
+                ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                break;
+            case 4: // mute - closes X15-Y0
+                CROSSPOINT_SWITCH_CONTROL(0, 3, 0);
+                CROSSPOINT_SWITCH_CONTROL(4, 0, 0);
+
+                CROSSPOINT_SWITCH_CONTROL(15, 0, 1);
+
+                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                break;
             }
         }
     }
