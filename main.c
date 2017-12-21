75861d93e52a4a215b060ba9bf67e464c1014914
Luís Patsko <luispatsko@yahoo.com.br>
2017-12-21 20:22:26 -0200

    Code developed for SPI testing. Function BUTTON_DETECTION modded so that it isn't blocking anymore.

--- main.c
+++ main.c
@@ -19,13 +19,13 @@ PC6 - AY1
 PC7 - AY2
 
 
-PC4 - Data
-PC5 - Strobe
-PB0 - CS
+PC4 - MOSI
+PC5 - SCK / Strobe
+PB0 - CS Crosspoint switch
+PC3 - CS Shift register
 
 PB1 - botão
 PC2 - LED1
-PC3 - LED2
 PC1 - LED debug
 
 PA4 - UART RX
@@ -46,17 +46,44 @@ struct {
     uint8_t MIDI_status;
     uint32_t MIDI_timeout;
     uint8_t MIDI_Message[3];
+
+    uint8_t SPI_status;
+    uint8_t SPI_debug;
+    uint8_t SPI_data_to_send[10];
 } TEST;
 
 char Message[] = "Loopback test!\r\n";
 
 uint8_t UART_Driver_Lock_ID = 0;
+uint8_t SPI_Driver_Lock_ID = 0;
 
+struct {
+    uint32_t Timeout;
+    uint8_t Status;
+} BUTTON;
 
 void UART_Driver_Callback (uint8_t data) {
 
 
 }
+
+void SPI_Driver_Callback (uint8_t data) {
+    ZHAL_GPIO_Config_t gpio_config = {
+        ZHAL_GPIO_OUTPUT,
+        ZHAL_GPIO_NORMAL,
+        DISABLE,
+        DISABLE,
+        DISABLE,
+        DISABLE
+    };
+
+    ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_3);
+    ZHAL_SPI_Driver_Close (SPI_Driver_Lock_ID, ZHAL_SPI_0);
+
+    // Crosspoint switch serial
+    ZHAL_GPIO_Config_Pin(ZHAL_GPIO_C, GPIO_PIN_4 | GPIO_PIN_5, &gpio_config);
+}
+
 
 void MCU_INIT () {
     ZHAL_GPIO_Config_t gpio_config = {
@@ -68,7 +95,6 @@ void MCU_INIT () {
         DISABLE
     };
     ZHAL_UART_Config_t uart_config;
-
 
     // Crosspoint switch addresses
     ZHAL_GPIO_Config_Pin(ZHAL_GPIO_A, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_6 | GPIO_PIN_7, &gpio_config);
@@ -94,6 +120,21 @@ void MCU_INIT () {
     uart_config.BaudRate = 38400;
 
     ZHAL_UART_Driver_Init (&UART_Driver_Lock_ID, ZHAL_UART_0, &uart_config, UART_Driver_Callback);
+
+}
+
+
+void SPI_CONFIG () {
+
+    ZHAL_SPI_Config_t spi_config;
+
+    spi_config.Mode = ZHAL_SPI_MODE_DEFAULT;
+    spi_config.MasterSlave = ZHAL_SPI_MODE_MASTER;
+    spi_config.PolarityPhase = ZHAL_SPI_POL_PHASE_0;
+    spi_config.WiredOR = DISABLE;
+    spi_config.BaudRate = 50000;
+
+    ZHAL_SPI_Driver_Init (&SPI_Driver_Lock_ID, ZHAL_SPI_0, &spi_config, SPI_Driver_Callback);
 
 }
 
@@ -176,14 +217,28 @@ void CROSSPOINT_SWITCH_CONTROL (unsigned char x, unsigned char y, unsigned char 
 }
 
 
-unsigned char BUTTON (ZHAL_GPIO_Port_t port, uint8_t pin) {
-    long i;
+unsigned char BUTTON_DETECTION (ZHAL_GPIO_Port_t port, uint8_t pin) {
     
-    if (ZHAL_GPIO_Read_Input(port, pin) == 0) {
-        for (i = 100000; i != 0; i--);
+    switch (BUTTON.Status) {
+    case 0:
         if (ZHAL_GPIO_Read_Input(port, pin) == 0) {
+            BUTTON.Timeout = 10000;
+            BUTTON.Status++;
+        }
+        break;
+    case 1:
+        if (BUTTON.Timeout != 0) {
+            BUTTON.Timeout--;
+        } else {
+            BUTTON.Status++;
+        }
+        break;
+    case 2:
+        if (ZHAL_GPIO_Read_Input(port, pin) == 0) {
+            BUTTON.Status = 0;
             return (1);
-        }        
+        }
+        break;
     }
     return (0);
 }
@@ -201,7 +256,7 @@ void main () {
     
     while (1){
 
-        if (BUTTON(ZHAL_GPIO_B, GPIO_PIN_1)) {
+        if (BUTTON_DETECTION(ZHAL_GPIO_B, GPIO_PIN_1)) {
             MATRIX.Status++;
             if (MATRIX.Status > 4) {
                 MATRIX.Status = 0;
@@ -213,7 +268,8 @@ void main () {
 
                 CROSSPOINT_SWITCH_CONTROL(0, 0, 1);
 
-                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                TEST.SPI_data_to_send[0] = 0xFF;
+                TEST.SPI_status = 2;
                 break;
             case 1: // effect 1 - closes X0-Y1 and X2-Y0
                 CROSSPOINT_SWITCH_CONTROL(0, 0, 0);
@@ -221,8 +277,8 @@ void main () {
                 CROSSPOINT_SWITCH_CONTROL(0, 1, 1);
                 CROSSPOINT_SWITCH_CONTROL(2, 0, 1);
 
-                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
-                ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_2);
+                TEST.SPI_data_to_send[0] = 0xFE;
+                TEST.SPI_status = 2;
                 break;
             case 2: // effect 2 - closes X0-Y2 and X3-Y0
                 CROSSPOINT_SWITCH_CONTROL(0, 1, 0);
@@ -231,8 +287,8 @@ void main () {
                 CROSSPOINT_SWITCH_CONTROL(0, 2, 1);
                 CROSSPOINT_SWITCH_CONTROL(3, 0, 1);
 
-                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
-                ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_3);
+                TEST.SPI_data_to_send[0] = 0xFD;
+                TEST.SPI_status = 2;
                 break;
             case 3: // effect 3 - closes X0-Y3 and X4-Y0
                 CROSSPOINT_SWITCH_CONTROL(0, 2, 0);
@@ -241,7 +297,8 @@ void main () {
                 CROSSPOINT_SWITCH_CONTROL(0, 3, 1);
                 CROSSPOINT_SWITCH_CONTROL(4, 0, 1);
 
-                ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                TEST.SPI_data_to_send[0] = 0xFC;
+                TEST.SPI_status = 2;
                 break;
             case 4: // mute - closes X15-Y0
                 CROSSPOINT_SWITCH_CONTROL(0, 3, 0);
@@ -249,17 +306,15 @@ void main () {
 
                 CROSSPOINT_SWITCH_CONTROL(15, 0, 1);
 
-                ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_2 | GPIO_PIN_3);
+                TEST.SPI_data_to_send[0] = 0xFF;
+                TEST.SPI_status = 2;
                 break;
             }
-#endif
-#if 0
-            TEST.MIDI_status = 1;
-            TEST.UART_status = 2;
 #endif
         }
 
         ZHAL_UART_Driver ();
+        ZHAL_SPI_Driver ();
 
 #if 0
         switch (TEST.UART_status) {
@@ -315,6 +370,7 @@ void main () {
         }
 #endif
 #if 0
+        // MIDI test - when the button is pressed, sends note on, waits some time and sends note off
         switch (TEST.MIDI_status) {
         case 1:
             TEST.MIDI_status++;
@@ -338,7 +394,8 @@ void main () {
             break;
         }
 #endif
-#if 1
+#if 0
+        // MIDI loopback test - receives data and resends it
         switch (TEST.MIDI_status) {
         case 0:
             if ((ZHAL_UART_Driver_Peek (UART_Driver_Lock_ID, &TEST.DataReceived) != 0) && (TEST.DataReceived == 0x0D)) {
@@ -360,6 +417,81 @@ void main () {
             break;
         }
 #endif
+
+#if 0
+        // Send byte through SPI when button is pressed
+        switch (TEST.SPI_status) {
+        case 1:
+            ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_3);
+            SPI_CONFIG();
+
+            ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "Start! SPI stat:", 16);
+            TEST.SPI_debug = ESPISTAT;
+            for (i = 0x80; i != 0; i >>= 1) {
+                if ((TEST.SPI_debug & i) != 0) {
+                    ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "1", 1);
+                } else {
+                    ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "0", 1);
+                }
+            }
+            ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "State:", 6);
+            TEST.SPI_debug = ESPISTATE;
+            for (i = 0x80; i != 0; i >>= 1) {
+                if ((TEST.SPI_debug & i) != 0) {
+                    ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "1", 1);
+                } else {
+                    ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "0", 1);
+                }
+            }
+            ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "\n", 1);
+            ZHAL_UART_Driver_Control (UART_Driver_Lock_ID, 1);
+
+            ZHAL_SPI_Driver_Put_Data (SPI_Driver_Lock_ID, TEST.SPI_data_to_send, 1);
+            ZHAL_SPI_Driver_Control (SPI_Driver_Lock_ID, 1);
+
+            TEST.SPI_status++;
+            break;
+        case 2:
+            break;
+        case 3:
+            ZHAL_GPIO_Set_Output(ZHAL_GPIO_C, GPIO_PIN_3);
+            ZHAL_SPI_Driver_Close (SPI_Driver_Lock_ID, ZHAL_SPI_0);
+
+            ZHAL_UART_Driver_Put_Data (UART_Driver_Lock_ID, "Message sent!\n", 14);
+            ZHAL_UART_Driver_Control (UART_Driver_Lock_ID, 1);
+
+            TEST.SPI_status = 0;
+            break;
+        }
+#endif
+
+
+        // SPI test - receive a byte from UART and sends to Shift register
+        switch (TEST.SPI_status) {
+        case 0:
+            if (ZHAL_UART_Driver_Get_Data(UART_Driver_Lock_ID, &TEST.DataReceived, 1) != 0) {
+                TEST.SPI_status = 1;
+            }
+            break;
+        case 1:
+            ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_3);
+            SPI_CONFIG();
+
+            ZHAL_SPI_Driver_Put_Data (SPI_Driver_Lock_ID, &TEST.DataReceived, 1);
+            ZHAL_SPI_Driver_Control (SPI_Driver_Lock_ID, 1);
+
+            TEST.SPI_status = 0;
+            break;
+        case 2: // from another source
+            ZHAL_GPIO_Reset_Output(ZHAL_GPIO_C, GPIO_PIN_3);
+            SPI_CONFIG();
+
+            ZHAL_SPI_Driver_Put_Data (SPI_Driver_Lock_ID, TEST.SPI_data_to_send, 1);
+            ZHAL_SPI_Driver_Control (SPI_Driver_Lock_ID, 1);
+
+            TEST.SPI_status = 0;
+            break;
+        }
     }
 }
 
